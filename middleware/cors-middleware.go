bfe9ac9074a8dbed6980c42281a12b7d90f0b127
aliefkhairulfz <aliefkhairulfz@gmail.com>
2026-01-30 18:20:19 +0700

    feat: add vercel cors

--- middleware/cors-middleware.go
+++ middleware/cors-middleware.go
@@ -7,7 +7,7 @@ import (
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://finance-app-be-next.vercel.app")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
